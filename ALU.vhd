1783f240130bc22ad83ef524aad3bf9af79ee55d
Khuna Chuan <chuan_8297@hotmail.com>
2018-05-04 11:23:26 +0700

    add split instruction code

--- ALU.vhd
+++ ALU.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
 use IEEE.std_logic_arith.all;
 
 entity ALU is
@@ -22,28 +23,39 @@ end ALU;
 architecture data_flow of ALU is
 
   signal res_temp: std_logic_vector(N-1 downto 0);
+  signal rt_temp: std_logic_vector(N-1 downto 0);
   signal carry: std_logic_vector(N downto 0);
   
   begin
   process(clk)
     begin
       
-      -- and
+      -- and  rd = rs and rt
       if ALUop = "00" then
         res_temp <= rs and rt;
       
-      -- or
+      -- or   rd = rs or rt
       elsif ALUop = "01" then
         res_temp <= rs or rt;
       
-      -- add
+      -- add  rd = rs + rt
       elsif ALUop = "10" then
         adder: for i in 0 to N-1 loop
           res_temp(i) <= (rs(i) xor rt(i)) xor carry(i);
           carry(i+1) <= (rs(i) and rt(i)) or (carry(i) and (rs(i) xor rt(i)));
         end loop;
         
+      -- sub  rd = rs + (-rt)
+      elsif ALUop = "11" then
+        -- 2's complement rt save in rt_temp
+        rt_temp <= (not(rt)) + 1;
         
+        sub: for i in 0 to N-1 loop         -- subber????? subtractor?
+          res_temp(i) <= (rs(i) xor rt_temp(i)) xor carry(i);
+          carry(i+1) <= (rs(i) and rt_temp(i)) or (carry(i) and (rs(i) xor rt_temp(i)));
+        end loop;
+    
+    
       end if;
       
       
